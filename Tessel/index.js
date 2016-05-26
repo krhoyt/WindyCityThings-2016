f5d97c24fd792fd0a33e4c86a247c3f83dd6475d
Kevin Hoyt <parkerkrhoyt@gmail.com>
2016-05-26 12:56:26 -0600

    Button, photocell, and LED.
    Starting to dial in on specific demo per board.  LED control from a web
    page.  Photocell values to a web page.  Button press to trigger Watson
    STT.  STT result will then control LED.

--- Tessel/index.js
+++ Tessel/index.js
@@ -1,23 +1,84 @@
 // MQTT for Watson IoT
 // JSON file for configuration
 // Path for file location on device
+// Tessel for hardware interaction
 var jsonfile = require( 'jsonfile' );
 var mqtt = require( 'mqtt' );
 var path = require( 'path' );
+var tessel = require( 'tessel' );
+
+// Application variables
+var bounce;
+var button;
+var client;
+var config;
+var interval;
+var led;
+var reading;
+var photocell;
+var pressed;
 
 // Load external authentication
-var config = jsonfile.readFileSync( path.join( __dirname, 'config.json' ) );
+config = jsonfile.readFileSync( path.join( __dirname, 'config.json' ) );
 
 // Connect to Watson IoT
-var client = mqtt.connect( config.host, {
+client = mqtt.connect( config.host, {
 	clientId: config.client,
 	password: config.password,
 	username: 'use-token-auth'
 } );
-var count = 0;
-var interval;
+
+// LED on pin 1
+// Treat as digital output
+// Turn off initially
+led = tessel.port.B.pin[1];
+led.output( 0 );
+
+// Photocell on pin 3
+// Treat as analog input
+photocell = tessel.port.B.pin[3];
+
+// Work with button on pin 5
+button = tessel.port.B.pin[5];
+reading = false;
+pressed = false;
+
+// Monitor button pin
+bounce = setInterval( function() {
+	// Only read if not already reading
+	if( !reading ) {
+		// Going to read
+		reading = true;
+		
+		// Asynchromouns button reading
+		button.read( function( err, value ) {
+			// Up
+			if( value === 0 ) {
+				pressed = false;
+			// Down
+			} else if( value === 1 ) {
+				// Only fire press once
+				if( !pressed ) {
+					pressed = true;
+					
+					// Publish value to Watson IoT
+					client.publish( 'iot-2/evt/button/fmt/json', JSON.stringify( {
+						pressed: pressed
+					} ) );					
+					
+					console.log( 'Pressed.' );
+				}
+			}
+			
+			// Finished reading
+			reading = false;
+		} );
+	}
+}, 100 );
 
 // Connected
+// Subscribe to LED commands
+// Start sending photocell events
 client.on( 'connect', function() {
 	console.log( 'Connected.' );
 	
@@ -26,17 +87,19 @@ client.on( 'connect', function() {
 		console.log( 'Subscribed.' );
 	} );
 	
-	// Start counting
-	// Once per second
+	// Send light value to clients
+	// Decoupled from reading	
 	interval = setInterval( function() {
-		count = count + 1;
+		// Read pin value
+		// Between 0 (dark) and 3.3 (light)
+		photocell.analogRead( function( err, value ) {
+			// Publish value to Watson IoT
+			client.publish( 'iot-2/evt/light/fmt/json', JSON.stringify( {
+				light: value
+			} ) );
 		
-		// Publish value to Watson IoT
-		client.publish( 'iot-2/evt/count/fmt/json', JSON.stringify( {
-			count: count
-		} ) );
-		
-		console.log( 'Count: ' + count );
+			console.log( 'Light: ' + value );					
+		} );		
 	}, 1000 );
 } );
 
@@ -47,6 +110,9 @@ client.on( 'message', function( topic, message, packet ) {
 	
 	// Object from JSON
 	data = JSON.parse( message );
+	
+	// Set LED state
+	led.output( data.led );
 	
 	console.log( 'Message: ' + data.led );
 } );
--- Web/windy.js
+++ Web/windy.js
@@ -5,7 +5,8 @@ var led;
 // Subscribe for count topic
 function doClientConnect( context ) {
     console.log( 'Connected.' );
-	client.subscribe( 'iot-2/type/Tessel/id/IBM/evt/count/fmt/json' );
+	client.subscribe( 'iot-2/type/Tessel/id/IBM/evt/light/fmt/json' );
+	client.subscribe( 'iot-2/type/Tessel/id/IBM/evt/button/fmt/json' );	
 }    
     
 // Unable to connect
@@ -14,13 +15,19 @@ function doClientFailure( context, code, message ) {
 }    
 
 // Message arrived
-function doCountArrived( message ) {
+function doMessageArrived( message ) {
 	var data = null;
-	var element = null;
-	
+
 	// Parse
 	data = JSON.parse( message.payloadString );
-	console.log( data );
+
+	// Subscribed to multiple topics
+	// Look for event name and direct accordingly
+	if( message.destinationName.indexOf( 'button' ) >= 0 ) {		
+		console.log( 'Button: ' + data.pressed );
+	} else if( message.destinationName.indexOf( 'light' ) >= 0 ) {
+		console.log( 'Light: ' + data.light );
+	}
 }	
 
 // Button to toggle LED clicked
@@ -29,7 +36,7 @@ function doToggleClick() {
 	var message = null;
 	
 	// Toggle LED state
-	led = !led;
+	led = led === 0 ? 1 : 0;
 	
 	// Build message
 	message = new Paho.MQTT.Message( JSON.stringify( {
@@ -45,7 +52,8 @@ function doWindowLoad() {
 	var button = null;
 	
 	// Initialize LED state
-	led = false;
+	// Off
+	led = 0;
 	
 	// Let the user change the LED state
 	button = document.querySelector( 'button' );
@@ -58,7 +66,7 @@ function doWindowLoad() {
             IOT_PORT, 
             IOT_CLIENT + Math.round( Math.random() * 1000 )
         );
-		client.onMessageArrived = doCountArrived;
+		client.onMessageArrived = doMessageArrived;
     } catch( error ) {
         console.log( 'Error: ' + error );
     }    
