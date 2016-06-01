9b6cc491f89768bd73c90f09a735d6d08573a513
Kevin Hoyt <parkerkrhoyt@gmail.com>
2016-06-01 10:59:47 -0600

    LED and button working.
    Formally implemented LED and button.  Photocell requires ADC, which
    requires an additional board (on order).

--- Edison/windy.js
+++ Edison/windy.js
@@ -1,14 +1,23 @@
+// MQTT for Watson IoT
+// JSON file for configuration
+// Path for file location on device
+// MRAA for hardware interaction
 var jsonfile = require( 'jsonfile' );
 var mraa = require( 'mraa' );
 var mqtt = require( 'mqtt' );
 var path = require( 'path' );
 
-var client;
-var config;
-var led;
+var bounce;			// Interval for button
+var button;			// Button pin
+var client;			// Watson IoT client
+var config;			// Authentication
+var led;			// LED pin
+var pressed;		// Button press
 
+// Load external authentication
 config = jsonfile.readFileSync( path.join( __dirname, 'config.json' ) );
 
+// Connect to Watson IoT
 client = mqtt.connect( config.host, {
     clientId: config.client,
     password: config.password,
@@ -16,23 +25,69 @@ client = mqtt.connect( config.host, {
   }
 );
 
-led = new mraa.Gpio( 13 );
+// LED
+// GP14 == 36 on SparkFun GPIO Block
+// Treat as digital output
+// Turn off initially
+led = new mraa.Gpio( 36 );
 led.dir( mraa.DIR_OUT );
+led.write( 0 );
 
+// Button
+// 1k resistor for pull down
+// GP13 == 14 on SparkFun GPIO Block
+button = new mraa.Gpio( 14 );
+button.dir( mraa.DIR_IN );
+pressed = false;
+
+// Monitor button pin
+bounce = setInterval( function() {
+	var value = null;
+	
+	// Read pin
+	// Digital signal (on or off)
+	value = button.read();
+
+	// Up (off)
+	if( value === 0 ) {
+		pressed = false;
+	// Down (on)
+	} else if ( value === 1 ) {
+		// Only fire press once
+		if( !pressed ) {
+			pressed = true;
+			
+			// Publish value to Watson IoT
+			client.publish( 'iot-2/evt/button/fmt/json', JSON.stringify( {
+				pressed: pressed
+			} ) );
+			
+			console.log( 'Pressed.' );
+		}
+	}
+}, 100 );
+
+// Connected
+// Subscribe to LED commands
 client.on( 'connect', function() {
-  console.log( 'Connected.' );
+	console.log( 'Connected.' );
 
-  client.subscribe( 'iot-2/cmd/led/fmt/json', function( err, granted ) {
-    console.log( 'Subscribed.' );
-  } );
+	// Subscribe to LED commands
+	client.subscribe( 'iot-2/cmd/led/fmt/json', function( err, granted ) {
+		console.log( 'Subscribed.' );
+	} );
 } );
 
+// Handle messages
+// LED command
 client.on( 'message', function( topic, message, packet ) {
-  var data = null;
+	var data = null;
 
-  data = JSON.parse( message );
+	// Object from JSON
+	data = JSON.parse( message );
 
-  led.write( data.led );
+	// Set LED state
+	led.write( data.led );
 
-  console.log( 'Message: ' + data.led );
+	console.log( 'Message: ' + data.led );
 } );
