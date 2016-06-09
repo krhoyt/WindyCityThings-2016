a9438574fc92b43d8f7f70c8a1376fcda1beff4f
Kevin Hoyt <parkerkrhoyt@gmail.com>
2016-06-09 15:18:28 -0600

    Added photocell readings.
    Using ADC board from SparkFun (retired as soon as I purchased it).
    Does some strange magic with I2C to get the analog value, then do some
    calculations on the value to translate it into the 0 - 2047 range.

--- Edison/windy.js
+++ Edison/windy.js
@@ -8,10 +8,14 @@ var mqtt = require( 'mqtt' );
 var path = require( 'path' );
 
 var bounce;			// Interval for button
+var bright;			// Interval for photocell
 var button;			// Button pin
 var client;			// Watson IoT client
+var command;		// I2C commands
 var config;			// Authentication
+var interval; 		// Interval for publish
 var led;			// LED pin
+var light; 			// Photocell reading
 var pressed;		// Button press
 
 // Load external authentication
@@ -31,6 +35,34 @@ client = mqtt.connect( config.host, {
 led = new mraa.Gpio( 36 );
 led.dir( mraa.DIR_OUT );
 led.write( 0 );
+
+// Photocell
+// Gets treated as I2C on ADC board
+// 330 resistor seems to range levels best
+command = parseInt( '1000001101000100', 2 );
+photocell = new mraa.I2c( 1 );
+photocell.address( 0x48 );
+
+bright = setInterval( function() {
+	var data = null;
+	var result = null;
+	
+	// Get reading
+	photocell.writeWordReg( 0x01, command );
+	data = photocell.readWordReg( 0 );
+	
+	// Process signal
+	result = 0;
+	result += ( data & 0XF000 ) >> 12; 
+	result += ( data & 0X00F0 ) << 4;
+	result += ( data & 0X000F ) << 4;
+			
+	if( result > 0x7FF ) {
+		result = -1 * ( ( 0x7FF * 2 + 2 ) + ( ~result ) )		
+	}
+
+	light = result;
+}, 100 );
 
 // Button
 // 1k resistor for pull down
@@ -75,6 +107,16 @@ client.on( 'connect', function() {
 	client.subscribe( 'iot-2/cmd/led/fmt/json', function( err, granted ) {
 		console.log( 'Subscribed.' );
 	} );
+	
+	// Send light value to clients
+	// Decoupled from reading	
+	interval = setInterval( function() {
+		client.publish( 'iot-2/evt/light/fmt/json', JSON.stringify( {
+			light: map( light, 0, 2047, 0, 100 )
+		} ) );
+
+		console.log( 'Light: ' + light );
+	}, 1000 );	
 } );
 
 // Handle messages
@@ -90,3 +132,9 @@ client.on( 'message', function( topic, message, packet ) {
 
 	console.log( 'Message: ' + data.led );
 } );
+
+// Linear transform
+// Similar to Arduino map function
+function map( x, in_min, in_max, out_min, out_max ) {
+	return ( x - in_min ) * ( out_max - out_min ) / ( in_max - in_min ) + out_min;
+}
