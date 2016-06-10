dd7aa014796e6514a0b59e6a17111c17337df9fa
Kevin Hoyt <parkerkrhoyt@gmail.com>
2016-06-10 15:01:29 -0600

    Start of UI refactor.
    Moving from single view to view of all boards at once.

---
+++ Web/boards.js
@@ -0,0 +1,29 @@
+var boards = [
+    {
+        image: 'imp.jpg',
+        light: [],
+        name: 'Electric Imp',
+        topic: 'Imp'
+    },
+    {
+        image: 'photon.jpg',        
+        light: [],        
+        name: 'Particle Photon',
+        topic: 'Photon'
+    }, {
+        image: 'electron.jpg',        
+        light: [],        
+        name: 'Particle Electron',
+        topic: 'Electron',    
+    }, {
+        image: 'tessel.jpg',
+        light: [],        
+        name: 'Tessel 2',
+        topic: 'Tessel'
+    }, {
+        image: 'edison.jpg',
+        light: [],        
+        name: 'Intel Edison',
+        topic: 'Edison'
+    }
+];
--- Web/windy.js
+++ Web/windy.js
@@ -1,67 +1,122 @@
 var client;
-var led;
+
+function chart( topic, value ) {
+    var board = null;
+    var path = null;
+
+    // Debug
+    console.log( 'Charting: ' + value );
+    
+    board = document.querySelector( '.board[data-topic="' + topic + '"]' );
+    path = board.querySelector( 'path' );
+}
+
+function emphasize( topic ) {
+    var board = null;
+    var name = null;
+    
+    board = document.querySelector( '.board[data-topic="' + topic + '"]' );    
+    name = board.getAttribute( 'data-name' );
+    
+    // Debug
+    console.log( 'Emphasize: ' + name )
+}
+
+// Button to toggle LED clicked
+// Send event to device
+function doBoardClick() {
+    var led = null;
+    var message = null;
+    var name = null;
+    var topic = null;
+    
+    // Get LED state
+    led = parseInt( this.getAttribute( 'data-led' ) );
+    
+	// Toggle LED state
+	led = led === 0 ? 1 : 0;
+    
+    // Persist LED state
+    this.setAttribute( 'data-led', led );
+	
+    // Get topic
+    topic = this.getAttribute( 'data-topic' );    
+    
+    // Get board name
+    name = this.getAttribute( 'data-name' );
+
+    // Debug
+    console.log( 'Setting ' + name + ' to: ' + led );    
+    
+	// Build message
+	message = new Paho.MQTT.Message( JSON.stringify( {
+		led: led
+	} ) );
+	message.destinationName = 'iot-2/type/' + topic + '/id/IBM/cmd/led/fmt/json';
+	
+	// Send
+	client.send( message );	    
+}
 
 // Connected to broker
 // Subscribe for count topic
 function doClientConnect( context ) {
+    // Debug
     console.log( 'Connected.' );
+    
+    // Subscribe to button and light events
 	client.subscribe( 'iot-2/type/+/id/IBM/evt/light/fmt/json' );
 	client.subscribe( 'iot-2/type/+/id/IBM/evt/button/fmt/json' );	
 }    
     
 // Unable to connect
 function doClientFailure( context, code, message ) {
-    console.log( 'Connection fail.' );
+    // Debug
+    console.log( 'Connection failed.' );
 }    
 
 // Message arrived
 function doMessageArrived( message ) {
 	var data = null;
-	var element = null;
+    var index = null;
 
 	// Parse
 	data = JSON.parse( message.payloadString );
 
-	// Subscribed to multiple topics
-	// Look for event name and direct accordingly
-	if( message.destinationName.indexOf( 'button' ) >= 0 ) {		
-		console.log( 'Button: ' + data.pressed );
-	} else if( message.destinationName.indexOf( 'light' ) >= 0 ) {
-		element = document.querySelector( '.darkness' );
-		element.style.opacity = 1 - ( data.light / 100 );
-		console.log( 'Light: ' + data.light );
-	}
+    // Find board
+    for( var b = 0; b < boards.length; b++ ) {
+        if( message.destinationName.indexOf( boards[b].topic ) >= 0 ) {
+            index = b;
+            break;
+        }
+    }    
+    
+    if( message.destinationName.indexOf( 'button' ) ) {
+        // Debug
+        console.log( 'Button on ' + boards[index].name + '.' );
+        
+        // Show button hit
+        emphasize( board.topic );
+    } else if( message.destinationName.indexOf( 'light' ) ) {
+        // Debug
+        console.log( 'Light on ' + boards[index].name + ': ' + data.light );        
+        
+        // Chart light values
+        chart( boards[index].topic, data.light );
+    }
+    
+    // Debug
+    console.log( data );    
 }	
 
-// Button to toggle LED clicked
-// Send event to device
-function doToggleClick() {
-	var message = null;
-	
-	// Toggle LED state
-	led = led === 0 ? 1 : 0;
-	
-	// Build message
-	message = new Paho.MQTT.Message( JSON.stringify( {
-		led: led
-	} ) );
-	message.destinationName = 'iot-2/type/Photon/id/IBM/cmd/led/fmt/json';
-	
-	// Send
-	client.send( message );	
-}
-
-function doWindowLoad() {
-	var button = null;
-	
-	// Initialize LED state
-	// Off
-	led = 0;
-	
-	// Let the user change the LED state
-	button = document.querySelector( '.darkness' );
-	button.addEventListener( 'click', doToggleClick );
-	
+function doWindowLoad() {	
+    var board = null;
+    var panels = null;
+    var template = null;
+    
+    // Debug
+    console.log( 'Loaded.' );
+    
 	// Instantiate client
     try {
         client = new Paho.MQTT.Client(
@@ -81,6 +136,26 @@ function doWindowLoad() {
         onSuccess: doClientConnect,
         onFailure: doClientFailure
     } );	
+    
+    // Get panel holder and template
+    panels = document.querySelector( '.panels' );
+    template = document.querySelector( '.board.template' );
+    
+    // Build panels
+    for( var b = 0; b < boards.length; b++ ) {
+        // Populate data attributes per board
+        board = template.cloneNode( true );
+        board.classList.remove( 'template' );
+        board.style.backgroundImage = 'url( ' + boards[b].image + ' )';
+        board.children[1].innerHTML = boards[b].topic;
+        board.setAttribute( 'data-name', boards[b].topic );
+        board.setAttribute( 'data-topic', boards[b].topic );        
+        board.addEventListener( 'click', doBoardClick );
+        
+        // Add to holder
+        panels.appendChild( board );
+    }
 }
 
+// Go
 window.addEventListener( 'load', doWindowLoad );
