a73f5d1018afea7bf6bb8958d48a2e21a690aa6b
Kevin Hoyt <parkerkrhoyt@gmail.com>
2016-06-13 13:15:37 -0600

    One screen for all devices.
    Tested with a single device.  Will chart light levels, tell the LED to
    turn on/off, and react to a button press.  Now for multiples at once.

--- Web/windy.js
+++ Web/windy.js
@@ -1,25 +1,61 @@
 var client;
 
-function chart( topic, value ) {
+function chart( topic, index ) {
     var board = null;
+    var line = null;
     var path = null;
+    var step = null;
 
     // Debug
-    console.log( 'Charting: ' + value );
+    console.log( 'Charting: ' + topic );
     
+    // Where to draw
     board = document.querySelector( '.board[data-topic="' + topic + '"]' );
     path = board.querySelector( 'path' );
+
+    // Movement for each data point
+    // One hundred data point possible
+    step = board.clientWidth / 100;
+    
+    // What to draw
+    // This one hundred is the vertical amount
+    for( var p = 0; p < boards[index].light.length; p++ ) {
+        if( line == null ) {
+            line = 'M 0 ' + ( 100 - boards[index].light[p] ) + ' ';
+        } else {
+            line = 
+                line + 'L ' + 
+                ( step * p ) + ' ' + 
+                ( 100 - boards[index].light[p] ) + ' ';
+        }
+    }
+    
+    // Draw
+    path.setAttribute( 'd', line );
 }
 
 function emphasize( topic ) {
     var board = null;
     var name = null;
+    var red = null;
     
+    // Get specific board element
+    // Name reference for debugging
     board = document.querySelector( '.board[data-topic="' + topic + '"]' );    
     name = board.getAttribute( 'data-name' );
     
     // Debug
-    console.log( 'Emphasize: ' + name )
+    console.log( 'Emphasize: ' + name );
+    
+    // Get inner emphasize element
+    // Set to full red
+    red = board.querySelector( '.emphasize' );
+    red.style.opacity = 1.0;
+    
+    // Tween opacity to zero
+    TweenMax.to( red, 1.0, {
+        opacity: 0    
+    } );
 }
 
 // Button to toggle LED clicked
@@ -91,22 +127,30 @@ function doMessageArrived( message ) {
         }
     }    
     
-    if( message.destinationName.indexOf( 'button' ) ) {
+    if( message.destinationName.indexOf( 'button' ) >= 0 ) {
         // Debug
         console.log( 'Button on ' + boards[index].name + '.' );
         
         // Show button hit
-        emphasize( board.topic );
-    } else if( message.destinationName.indexOf( 'light' ) ) {
+        emphasize( boards[index].topic );
+    } else if( message.destinationName.indexOf( 'light' ) >= 0) {
         // Debug
         console.log( 'Light on ' + boards[index].name + ': ' + data.light );        
         
+        // Limit display to one-hundred data points
+        if( boards[index].light.length == 100 ) {
+            boards[index].light.splice( 0, 1 );
+        }
+        
+        // Add new data point
+        boards[index].light.push( data.light );        
+        
         // Chart light values
-        chart( boards[index].topic, data.light );
+        chart( boards[index].topic, index );
     }
     
     // Debug
-    console.log( data );    
+    // console.log( data );    
 }	
 
 function doWindowLoad() {	
